cf50d909f1203efe4d18a03efbc98d2d20f56d6f
se1L0rrr <idrisovhasan@mail.ru>
2024-09-29 22:18:16 +0500

    sdelal 2nd zadachky

---
+++ ex_idr/ex_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int n = Math.Max(a, Math.Max(b, c));
+            Console.WriteLine($"максимальное:{n}");
+        }
+    }
+}
